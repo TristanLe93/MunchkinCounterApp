aff94551394c8f9f87795b5af4748baa6f22ba9c
Tristan Le <tristan.le@student.qut.edu.au>
2014-10-29 10:40:51 +1000

    updated UI

--- MunchkinCounter/app/src/main/java/com/example/tristan/munchkincounter/ListAdapter.java
+++ MunchkinCounter/app/src/main/java/com/example/tristan/munchkincounter/ListAdapter.java
@@ -172,8 +172,6 @@ public class ListAdapter extends BaseAdapter {
             holder.playerNameView = (TextView)convertView.findViewById(R.id.playerNameText);
             holder.levelView = (TextView)convertView.findViewById(R.id.levelNumberText);
             holder.totalView = (TextView)convertView.findViewById(R.id.totalNumberText);
-            holder.levelText = (TextView)convertView.findViewById(R.id.levelText);
-            holder.totalText = (TextView)convertView.findViewById(R.id.totalText);
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder)convertView.getTag();
@@ -188,10 +186,7 @@ public class ListAdapter extends BaseAdapter {
         // set text font
         Typeface tf = FontCache.get("fonts/quasimodo.ttf", layoutInflater.getContext());
         holder.playerNameView.setTypeface(tf);
-        holder.levelView.setTypeface(tf);
         holder.totalView.setTypeface(tf);
-        holder.levelText.setTypeface(tf);
-        holder.totalText.setTypeface(tf);
 
         return convertView;
     }
@@ -203,7 +198,5 @@ public class ListAdapter extends BaseAdapter {
         TextView playerNameView;
         TextView levelView;
         TextView totalView;
-        TextView levelText;
-        TextView totalText;
     }
 }
